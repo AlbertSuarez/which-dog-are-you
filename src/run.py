e2a37098e2965ec58ee8ff3a67c2f1c365970265
Albert Suarez <alsumo95@gmail.com>
2020-04-04 22:32:46 +0200

    Added flag for showing result

--- src/run.py
+++ src/run.py
@@ -3,6 +3,8 @@ import os
 import shutil
 import requests
 
+from PIL import Image
+
 from src import DATA_FOLDER, IMAGES_LIST_FILE_NAME, INDEX_FILE_NAME, INPUT_IMAGE_FILE_NAME, OUTPUT_IMAGE_FILE_NAME
 from src.helper import keras, index
 
@@ -15,10 +17,11 @@ def parse_args():
     parser.add_argument('--index_file_path', type=str, default=os.path.join(DATA_FOLDER, INDEX_FILE_NAME))
     parser.add_argument('--input_image_path', type=str, default=os.path.join(DATA_FOLDER, INPUT_IMAGE_FILE_NAME))
     parser.add_argument('--output_image_path', type=str, default=os.path.join(DATA_FOLDER, OUTPUT_IMAGE_FILE_NAME))
+    parser.add_argument('--show', action='store_true')
     return parser.parse_args()
 
 
-def run(image_path, image_url, images_list_path, index_file_path, input_image_path, output_image_path):
+def run(image_path, image_url, images_list_path, index_file_path, input_image_path, output_image_path, show):
     # Check input
     if bool(image_path) == bool(image_url):
         print('You can only specify Image Path (x)or Image URL')
@@ -45,10 +48,15 @@ def run(image_path, image_url, images_list_path, index_file_path, input_image_pa
     index.search(features, index_file_path, images_list_path, output_image_path)
     print(f'Image result: {output_image_path}')
 
+    # Show
+    if show:
+        img = Image.open(output_image_path)
+        img.show()
+
 
 if __name__ == '__main__':
     args = parse_args()
     run(
         args.image_path, args.image_url, args.images_list_path, args.index_file_path,
-        args.input_image_path, args.output_image_path
+        args.input_image_path, args.output_image_path, args.show
     )
